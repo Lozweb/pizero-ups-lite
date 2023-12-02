c5d1b6c5b9f75db3c3990f6921edecfc9e46cd7d
BuildTools <aelozweb@gmail.com>
2023-12-02 21:41:24 +0100

    try to fix all values = 0

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import struct
 import time
+import RPi.GPIO as GPIO
 import smbus
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(4, GPIO.IN)
+
+print(" ")
 
 
 def readVoltage(smbus_value):
@@ -21,6 +28,22 @@ def readCapacity(smbus_value):
 
 bus = smbus.SMBus(1)
 
+
+def QuickStart(smbus_value):
+    address = 0x36
+    smbus_value.write_word_data(address, 0x06, 0x4000)
+
+
+def PowerOnReset(smbus_value):
+    address = 0x36
+    smbus_value.write_word_data(address, 0xfe, 0x0054)
+
+
+#PowerOnReset(bus)
+QuickStart(bus)
+
+print("Initialize the MAX17040 ......")
+
 while True:
     print("++++++++++++++++++++")
     print("Voltage:%5.2fV" % readVoltage(bus))
@@ -29,5 +52,12 @@ while True:
         print("Battery FULL")
     if readCapacity(bus) < 5:
         print("Battery LOW")
+
+    if GPIO.input(4) == GPIO.HIGH:
+        print("Power Adapter Plug In ")
+
+    if GPIO.input(4) == GPIO.LOW:
+        print("Power Adapter Unplug")
+
     print("++++++++++++++++++++")
     time.sleep(2)

9d4255d55ed93869655c40f6bc76a2387565e1cf
BuildTools <aelozweb@gmail.com>
2023-12-02 22:02:25 +0100

    remove log debug, implement loop display 1sec

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import smbus
 import struct
 import time
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
 
 ADDRESS = 0x36
 
@@ -11,11 +8,8 @@ ADDRESS = 0x36
 def readVoltage(bus):
     try:
         read = bus.read_word_data(ADDRESS, 1)
-        logging.info("voltage read: " + str(read))
         swapped = struct.unpack("<H", struct.pack(">H", read))[0]
-        logging.info("voltage swapped: " + str(swapped))
         voltage = swapped * 1.25 / 1000 / 16
-        logging.info("voltage return: " + str(voltage))
         return voltage
     except:
         return 0.0
@@ -24,11 +18,8 @@ def readVoltage(bus):
 def readCapacity(bus):
     try:
         read = bus.read_word_data(ADDRESS, 3)
-        logging.info("capacity read: " + str(read))
         swapped = struct.unpack("<H", struct.pack(">H", read))[0]
-        logging.info("capacity swapped: " + str(swapped))
         capacity = swapped / 256
-        logging.info("capacity return: " + str(capacity))
         return capacity
     except:
         return 0.0
@@ -45,8 +36,10 @@ def PowerOnReset(bus):
 
 
 bus = smbus.SMBus(1)
-# PowerOnReset(bus)
+PowerOnReset(bus)
 QuickStart(bus)
 
-print("Voltage: %5.2fV" % readVoltage(bus))
-print("Capacity: %5i%%" % readCapacity(bus))
+while True:
+    print("Voltage: %5.2fV" % readVoltage(bus))
+    print("Capacity: %5i%%" % readCapacity(bus))
+    time.sleep(1)
